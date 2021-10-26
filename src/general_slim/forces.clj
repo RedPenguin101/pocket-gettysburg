f0cb054f56766c28d2669d0043d9e24514007dae
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-10-26 18:50:17 +0100

    pull out unit templates

--- src/general_slim/forces.clj
+++ src/general_slim/forces.clj
@@ -1,92 +1,9 @@
 (ns general-slim.forces
   (:require [general-slim.utils :refer [map-vals]]))
 
-(def unit-templates
-  {:infantry {:unit-type :infantry
-              :hp 10 :max-hp 10
-              :move-points 3 :max-move-points 3
-              :movement-table {:field 1
-                               :road 0.5
-                               :trees 1
-                               :mountains 2}
-              :can-attack true
-              :offensive-power 55
-              :terrain-bonus {:on-attack {:defense {:road 0
-                                                    :field 0.5
-                                                    :trees 1
-                                                    :mountains 2}
-                                          :damage {:road 0
-                                                   :field 0
-                                                   :trees 1
-                                                   :mountains 3}}
-                              :on-defence {:defense {:road 0
-                                                     :field 1
-                                                     :trees 2
-                                                     :mountains 4}
-                                           :damage {:road 0
-                                                    :field 0
-                                                    :trees 2
-                                                    :mountains 2}}}}
-   :artillery {:unit-type :artillery
-               :hp 10 :max-hp 10
-               :move-points 2 :max-move-points 2
-               :movement-table {:field 1
-                                :road 0.5
-                                :trees 2
-                                :mountains 3}
-               :can-attack true
-               :offensive-power 55
-               :terrain-bonus {:on-attack {:defense {:road 0
-                                                     :field 0.5
-                                                     :trees 1
-                                                     :mountains 2}
-                                           :damage {:road 0
-                                                    :field 0
-                                                    :trees 1
-                                                    :mountains 3}}
-                               :on-defence {:defense {:road 0
-                                                      :field 1
-                                                      :trees 2
-                                                      :mountains 4}
-                                            :damage {:road 0
-                                                     :field 0
-                                                     :trees 2
-                                                     :mountains 2}}}}
-   :cavalry {:unit-type :cavalry
-             :hp 10 :max-hp 10
-             :move-points 3 :max-move-points 3
-             :movement-table {:road 0.5
-                              :field 1
-                              :trees 3
-                              :mountains 4}
-             :can-attack true
-             :offensive-power 75
-             :terrain-bonus {:on-attack {:defense {:road 0
-                                                   :field 0.5
-                                                   :trees 1
-                                                   :mountains 2}
-                                         :damage {:road 0
-                                                  :field 0
-                                                  :trees 1
-                                                  :mountains 3}}
-                             :on-defence {:defense {:road 0
-                                                    :field 1
-                                                    :trees 2
-                                                    :mountains 4}
-                                          :damage {:road 0
-                                                   :field 0
-                                                   :trees 2
-                                                   :mountains 2}}}}})
-
-(defn make-unit [type side id pos]
+(defn make-unit [type side id pos unit-templates]
   (assoc (type unit-templates)
          :id id :side side :position pos))
-
-(def red {:units {:inf1 (make-unit :infantry :red :inf1 [2 2])
-                  :inf2 (make-unit :infantry :red :inf2 [3 3])}})
-
-(def blue {:units {:inf1 (make-unit :infantry :blue :inf1 [7 7])
-                   :inf2 (make-unit :infantry :blue :inf2 [8 8])}})
 
 (defn units
   "Returns a sequence of every unit
@@ -116,6 +33,3 @@
 
 (defn defence-value [unit terrain]
   (get-in unit [:terrain-defense terrain]))
-
-(comment
-  (refresh-units (:units red)))
--- src/general_slim/scenarios.clj
+++ src/general_slim/scenarios.clj
@@ -4,22 +4,23 @@
             [general-slim.forces :refer [make-unit]]
             [general-slim.field :refer [field-size]]))
 
-(defn mu [side]
+(defn mu [side unit-templates]
   (fn [[type name pos]]
-    [name (make-unit type side name pos)]))
+    [name (make-unit type side name pos unit-templates)]))
 
-(defn make-units [side units]
-  {:units (into {} (map (mu side) units))})
+(defn make-units [side units unit-templates]
+  {:units (into {} (map (mu side unit-templates) units))})
 
 (defn add-field-size [game-state]
   (assoc game-state :field-size (field-size (:field game-state))))
 
 (defn load-scenario [scenario-name]
-  (let [{:keys [map red blue turn turn-number]}
+  (let [unit-templates (edn/read-string (slurp (str "resources/units.edn")))
+        {:keys [map red blue turn turn-number]}
         (edn/read-string (slurp (str "resources/scenarios/" scenario-name ".edn")))]
     (add-field-size {:field (load-map map)
                      :turn turn :turn-number turn-number
-                     :red (make-units :red red)
-                     :blue (make-units :blue blue)})))
+                     :red (make-units :red red unit-templates)
+                     :blue (make-units :blue blue unit-templates)})))
 
 (dissoc (load-scenario "basic") :field)
