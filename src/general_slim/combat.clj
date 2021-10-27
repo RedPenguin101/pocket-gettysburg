8541a5253562f90f4d35b9c7a942d5e1e6976e2a
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-10-27 11:25:54 +0100

    Receiving terrain impacts hit rate

--- src/general_slim/combat.clj
+++ src/general_slim/combat.clj
@@ -1,10 +1,16 @@
 (ns general-slim.combat
   (:require [general-slim.utils :refer [average rng-int zero-floored-minus]]))
 
+(def hit-rates
+  {:field 0.2
+   :trees 0.1
+   :mountains 0.2
+   :road 0.2})
+
 (defn calculate-hits
-  [shots enemy-men]
+  [shots enemy-men hit-rate]
   (count (reduce (fn [s t]
-                   (if (< (rand) 0.2)
+                   (if (< (rand) hit-rate)
                      (conj s t)
                      s))
                  #{}
@@ -24,14 +30,20 @@
            (<= (:soldiers defending-unit) 0))
      [(dissoc assaulting-unit :terrain) (dissoc defending-unit :terrain)]
      (let [d-cas (calculate-hits (:soldiers assaulting-unit)
-                                 (:soldiers defending-unit))
+                                 (:soldiers defending-unit)
+                                 (hit-rates (:terrain defending-unit)))
            a-cas (calculate-hits (:soldiers defending-unit)
-                                 (:soldiers assaulting-unit))]
+                                 (:soldiers assaulting-unit)
+                                 (hit-rates (:terrain assaulting-unit)))]
        (println "Volley" (- 6 rounds) ":")
        (println (:soldiers assaulting-unit) (name (:side assaulting-unit))
-                "fired on" (:soldiers defending-unit) (name (:side defending-unit)) "causing" d-cas "casualties")
+                "fired on" (:soldiers defending-unit) (name (:side defending-unit))
+                "in" (name (:terrain defending-unit))
+                "causing" d-cas "casualties")
        (println (:soldiers defending-unit) (name (:side defending-unit))
-                "fired on" (:soldiers assaulting-unit) (name (:side assaulting-unit)) "causing" a-cas "casualties")
+                "fired on" (:soldiers assaulting-unit) (name (:side assaulting-unit))
+                "in" (name (:terrain assaulting-unit))
+                "causing" a-cas "casualties")
        (recur (update assaulting-unit :soldiers zero-floored-minus a-cas)
               (update defending-unit :soldiers zero-floored-minus d-cas)
               (dec rounds))))))
