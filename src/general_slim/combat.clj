dc65fd0a8027d5b844da0a44d5bf348224e9cf05
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-10-27 11:31:14 +0100

    clean up resolve combat

--- src/general_slim/combat.clj
+++ src/general_slim/combat.clj
@@ -21,29 +21,28 @@
     [:force-proportion (float (/ x 1000))
      :casualty-rate (int (* 100 (/ (average (repeatedly 1000 #(calculate-hits 1000 x))) x)))]))
 
+(defn print-combat-report [volleying-unit other-unit casualties]
+  (println (:soldiers volleying-unit) (name (:side volleying-unit))
+           "fired on" (:soldiers other-unit) (name (:side other-unit))
+           "in" (name (:terrain other-unit))
+           "causing" casualties "casualties"))
+
 (defn resolve-combat
-  ([assaulting-unit defending-unit]
-   (resolve-combat assaulting-unit defending-unit 5))
-  ([assaulting-unit defending-unit rounds]
+  ([assaulting-unit defending-unit] (resolve-combat assaulting-unit defending-unit 5))
+  ([a-unit d-unit rounds]
    (if (or (zero? rounds)
-           (<= (:soldiers assaulting-unit) 0)
-           (<= (:soldiers defending-unit) 0))
-     [(dissoc assaulting-unit :terrain) (dissoc defending-unit :terrain)]
-     (let [d-cas (calculate-hits (:soldiers assaulting-unit)
-                                 (:soldiers defending-unit)
-                                 (hit-rates (:terrain defending-unit)))
-           a-cas (calculate-hits (:soldiers defending-unit)
-                                 (:soldiers assaulting-unit)
-                                 (hit-rates (:terrain assaulting-unit)))]
+           (<= (:soldiers a-unit) 0)
+           (<= (:soldiers d-unit) 0))
+     [(dissoc a-unit :terrain) (dissoc d-unit :terrain)]
+     (let [d-cas (calculate-hits (:soldiers a-unit)
+                                 (:soldiers d-unit)
+                                 (hit-rates (:terrain d-unit)))
+           a-cas (calculate-hits (:soldiers d-unit)
+                                 (:soldiers a-unit)
+                                 (hit-rates (:terrain a-unit)))]
        (println "Volley" (- 6 rounds) ":")
-       (println (:soldiers assaulting-unit) (name (:side assaulting-unit))
-                "fired on" (:soldiers defending-unit) (name (:side defending-unit))
-                "in" (name (:terrain defending-unit))
-                "causing" d-cas "casualties")
-       (println (:soldiers defending-unit) (name (:side defending-unit))
-                "fired on" (:soldiers assaulting-unit) (name (:side assaulting-unit))
-                "in" (name (:terrain assaulting-unit))
-                "causing" a-cas "casualties")
-       (recur (update assaulting-unit :soldiers zero-floored-minus a-cas)
-              (update defending-unit :soldiers zero-floored-minus d-cas)
+       (print-combat-report a-unit d-unit d-cas)
+       (print-combat-report d-unit a-unit a-cas)
+       (recur (update a-unit :soldiers zero-floored-minus a-cas)
+              (update d-unit :soldiers zero-floored-minus d-cas)
               (dec rounds))))))
