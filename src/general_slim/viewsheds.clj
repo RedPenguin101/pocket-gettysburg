003695ec92537682588cee3dd0f09ee49756ac3e
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-10-30 20:01:58 +0100

    stupid viewshed increase thing

--- src/general_slim/viewsheds.clj
+++ src/general_slim/viewsheds.clj
@@ -3,7 +3,7 @@
             [general-slim.forces :as forces]
             [general-slim.field :as field]))
 
-(defn paths [loc]
+(defn paths4 [loc]
   [(mapv #(coord+ loc %) [[0 -1] [0 -2] [0 -3] [0 -4]])
    (mapv #(coord+ loc %) [[1 -1] [1 -2] [1 -3]])
    (mapv #(coord+ loc %) [[1 -1] [2 -2]])
@@ -23,27 +23,29 @@
 
 (defn paths5 [loc]
   [(mapv #(coord+ loc %) [[0 -1] [0 -2] [0 -3] [0 -4] [0 -5]])
-
-
-   (mapv #(coord+ loc %) [[1 -1] [2 -2] [3 -3]])
-
-
+   (mapv #(coord+ loc %) [[0 -1] [0 -2] [0 -3] [1 -3] [1 -4]])
+   (mapv #(coord+ loc %) [[0 -1] [1 -1] [1 -2] [2 -2] [2 -3]])
+   (mapv #(coord+ loc %) [[1 -1] [2 -2]])
+   (mapv #(coord+ loc %) [[1 0] [1 -1] [2 -1] [2 -2] [3 -2]])
+   (mapv #(coord+ loc %) [[1 0] [2 0] [3 0] [3 1] [4 1]])
    (mapv #(coord+ loc %) [[1 0] [2 0] [3 0] [4 0] [5 0]])
-
-
-   (mapv #(coord+ loc %) [[1 1] [2 2] [3 3]])
-
-
+   (mapv #(coord+ loc %) [[1 0] [2 0] [3 0] [3 -1] [4 -1]])
+   (mapv #(coord+ loc %) [[1 0] [1 1] [2 1] [2 2] [3 2]])
+   (mapv #(coord+ loc %) [[1 1] [2 2]])
+   (mapv #(coord+ loc %) [[0 1] [1 1] [1 2] [2 2] [2 3]])
+   (mapv #(coord+ loc %) [[0 1] [0 2] [0 3] [1 3] [1 4]])
    (mapv #(coord+ loc %) [[0 1] [0 2] [0 3] [0 4] [0 5]])
-
-
-   (mapv #(coord+ loc %) [[-1 1] [-2 2] [-3 3]])
-
-
+   (mapv #(coord+ loc %) [[0 1] [0 2] [0 3] [-1 3] [-1 4]])
+   (mapv #(coord+ loc %) [[0 1] [-1 1] [-1 2] [-2 2] [-2 3]])
+   (mapv #(coord+ loc %) [[-1 1] [-2 2]])
+   (mapv #(coord+ loc %) [[-1 0] [-1 1] [-2 1] [-2 2] [-3 2]])
+   (mapv #(coord+ loc %) [[-1 0] [-2 0] [-3 0] [-3 1] [-4 1]])
    (mapv #(coord+ loc %) [[-1 0] [-2 0] [-3 0] [-4 0] [-5 0]])
-
-
-   (mapv #(coord+ loc %) [[-1 -1] [-2 -2] [-3 -3]])])
+   (mapv #(coord+ loc %) [[-1 0] [-2 0] [-3 0] [-3 -1] [-4 -1]])
+   (mapv #(coord+ loc %) [[-1 0] [-1 -1] [-2 -1] [-2 -2] [-3 -2]])
+   (mapv #(coord+ loc %) [[-1 -1] [-2 -2]])
+   (mapv #(coord+ loc %) [[0 -1] [-1 -1] [-1 -2] [-2 -2] [-2 -3]])
+   (mapv #(coord+ loc %) [[0 -1] [0 -2] [0 -3] [-1 -3] [-1 -4]])])
 
 (defn walk-path [path loc tile-terrain]
   (reduce (fn [out-path next-tile]
@@ -54,11 +56,11 @@
           []
           path))
 
-(defn viewshed [loc tmap]
-  (conj (set (mapcat #(walk-path % loc tmap) (paths loc))) loc))
+(defn viewshed [loc my-terrain tmap]
+  (conj (set (mapcat #(walk-path % loc tmap) (if (= :mountains my-terrain) (paths5 loc) (paths4 loc)))) loc))
 
 (defn calculate-viewsheds [game-state unit-loc]
-  (viewshed unit-loc (field/terrain-map (:field game-state))))
+  (viewshed unit-loc (field/terrain-at (:field game-state) unit-loc) (field/terrain-map (:field game-state))))
 
 (defn add-viewsheds [game-state]
   (->> (:turn game-state)
