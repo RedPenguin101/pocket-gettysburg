d52d978d9ae03edeab44d7fc438b59c0d7f87624
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-11-03 12:57:34 +0000

    tidy

--- dev/user.clj
+++ dev/user.clj
@@ -2,11 +2,13 @@
   (:require [portal.api :as p]
             [general-slim.ui :as ui]))
 
+(def debug ui/debug)
+
 (defn start! []
   (def p (p/open {:launcher :vs-code}))
   (add-tap #'p/submit))
 
 (comment
   (start!)
-  (tap> @general-slim.ui/debug)
+  (tap> @debug)
   (prn @p))
