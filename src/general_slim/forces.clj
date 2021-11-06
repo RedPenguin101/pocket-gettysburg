5a49b3236f91d9ebd4bc9021933438c78972c1df
RedPenguin101 <allaboutthatmace1789@gmail.com>
2021-11-06 20:58:40 +0000

    redo forces

--- src/general_slim/forces.clj
+++ src/general_slim/forces.clj
@@ -1,49 +1,77 @@
 (ns general-slim.forces
-  (:require [general-slim.utils :refer [map-vals in-view?]]))
+  (:require [general-slim.utils :refer [map-vals]]))
+
+(require '[clojure.test :refer [deftest is]])
 
 (defn make-unit [type side id name short pos unit-templates]
   (assoc (type unit-templates)
          :id id :side side :position pos :unit-name name :short-name short))
 
-(defn units
-  "Returns a sequence of every unit
-   The two arity version returns only units of one side"
-  ;; I think the assoc side is redundent now
-  ([game-state]
-   (concat (map #(assoc % :side :red) (vals (get-in game-state [:red :units])))
-           (map #(assoc % :side :blue) (vals (get-in game-state [:blue :units])))))
-  ([game-state side]
-   (map #(assoc % :side side) (vals (get-in game-state [side :units])))))
+;; Operations on a single unit
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn units-map
-  [game-state]
-  (merge (get-in game-state [:red :units]) (get-in game-state [:blue :units])))
-
-(defn occupied-grids
-  ([game-state] (set (map :position (units game-state))))
-  ([game-state side] (set (map :position (units game-state side)))))
-
-(defn unit-in-square
-  "Returns the unit occupying the square, or nil if none"
-  [game-state square]
-  (first (filter #(= square (:position %)) (units game-state))))
-
-(defn can-move? [unit]
-  (> (:move-points unit) 0))
-
-(defn refresh-units [unit-map]
-  (->> unit-map
-       (map-vals #(assoc % :move-points (:max-move-points %)
-                         :move-over false))))
+(defn reset [unit]
+  (assoc unit
+         :move-points (:max-move-points unit)
+         :move-over false))
 
 (defn defence-value [unit terrain]
   (get-in unit [:terrain-defense terrain]))
 
-(defn unit-in-view? [unit camera]
-  (in-view? camera (:position unit)))
+;; Operations on a map of units
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn units-at-locations [game-state locations]
-  (into {} (filter (fn [[_ unit]] (locations (:position unit))) (units-map game-state))))
+(defn reset [unit-map] (map-vals reset unit-map))
 
-(defn unit-with-id [game-state unit-id]
-  (get (units-map game-state) unit-id))
+(defn filter-side [side]
+  (fn  [units-map]
+    (into {} (filter (fn [[_ unit]] (= side (:side unit))) units-map))))
+
+(deftest y
+  (let [units {:abc {:id :abc :side :red :position [1 1]}
+               :def {:id :def :side :blue :position [2 2]}}]
+    (is (= (select-keys units [:abc])
+           ((filter-side :red) units)))
+    (is (= (select-keys units [:def])
+           ((filter-side :blue) units)))
+    (is (= {}
+           ((filter-side :green) units)))))
+
+;; Various ways to get units from a game-state
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn all
+  "Returns a map of every id->unit
+   The two arity version returns only units of one side"
+  ([game-state] (:units game-state))
+  ([game-state side] ((filter-side side) (all game-state))))
+
+(defn by-location
+  ([game-state] (into {} (map (fn [unit] [(:position unit) unit]) (vals (all game-state)))))
+  ([game-state side] ((filter-side side) (by-location game-state))))
+
+(defn at-locations [game-state locations]
+  (into {} (filter (fn [[k _v]] (locations k)) (by-location game-state))))
+
+(defn with-ids [game-state ids]
+  (into {} (filter (fn [[k _v]] (ids k)) (all game-state))))
+
+(deftest x
+  (let [gs {:units {:abc {:id :abc :side :red :position [1 1]}
+                    :def {:id :def :side :blue :position [2 2]}}}]
+    (is (= (:units gs)
+           (all gs)))
+    (is (= (select-keys (:units gs) [:abc])
+           (all gs :red)))
+    (is (= (select-keys (:units gs) [:def])
+           (all gs :blue)))
+    (is (= {[1 1] (:abc (:units gs))
+            [2 2] (:def (:units gs))}
+           (by-location gs)))
+    (is (= {[1 1] (:abc (:units gs))}
+           (by-location gs :red)))
+    (is (= {[2 2] (:def (:units gs))}
+           (by-location gs :blue)))
+    (is (= {[1 1] (:abc (:units gs))
+            [2 2] (:def (:units gs))}
+           (at-locations gs #{[1 1] [2 2]})))))
